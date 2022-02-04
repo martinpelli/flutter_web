7a8414073396da64975ba8ab9bc94039796089ac
Martin Pellicer <martinpelli@outlook.com>
2022-02-03 22:07:07 -0300

    Fluro navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web/locator.dart';
-import 'package:flutter_web/router/route_generator.dart';
+//import 'package:flutter_web/router/route_generator.dart';
+import 'package:flutter_web/router/router.dart';
 import 'package:flutter_web/services/navigation_service.dart';
 import 'package:flutter_web/ui/layout/main_layout_page.dart';
 
 void main() {
   setupLocator();
+  Flurorouter.configureRoutes();
   runApp(MyApp());
 }
 
@@ -15,8 +17,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: '/stateful',
-      onGenerateRoute: RouteGenerator.generateRoute,
+      initialRoute: '/',
+      //onGenerateRoute: RouteGenerator.generateRoute,
+      onGenerateRoute: Flurorouter.router.generator,
       navigatorKey: locator<NavigationService>().navigatorKey,
       builder: (_, child) {
         return MainLayoutPage(
---
+++ lib/router/router.dart
@@ -0,0 +1,27 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter_web/ui/views/counter_provider_view.dart';
+import 'package:flutter_web/ui/views/counter_view.dart';
+import 'package:flutter_web/ui/views/view_404.dart';
+
+class Flurorouter {
+  static final FluroRouter router = FluroRouter();
+
+  static void configureRoutes() {
+    router.define('/',
+        handler: _counterHandler, transitionType: TransitionType.fadeIn);
+    router.define('/stateful',
+        handler: _counterHandler, transitionType: TransitionType.fadeIn);
+    router.define('/provider',
+        handler: _counterProviderHandler,
+        transitionType: TransitionType.fadeIn);
+    router.notFoundHandler = _pageNotFound;
+  }
+
+  static Handler _counterHandler =
+      Handler(handlerFunc: (context, params) => CounterView());
+
+  static Handler _counterProviderHandler =
+      Handler(handlerFunc: (context, params) => CounterProviderView());
+
+  static Handler _pageNotFound = Handler(handlerFunc: (_, __) => View404());
+}
